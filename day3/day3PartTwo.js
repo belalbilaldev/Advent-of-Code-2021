1c3fab6699f49912b55e9e9eb4b9ece79364ab95
unknown <belal.bilal.dev@gmail.com>
2021-12-03 23:27:39 +0100

    day3

---
+++ day3/day3PartTwo.js
@@ -0,0 +1,46 @@
+console.time()
+const fs = require('fs')
+const input=fs.readFileSync('./input','utf-8').split(/\r?\n/).map((payload)=>payload)
+let table1=input;
+let table2 =input
+
+
+const calcule=(input,index)=>{
+    let height='';let lower=''
+    let countZero = 0;let countOne = 0
+    let values=[]
+          input.forEach(data=>{(data[index]==='0')?countZero++:countOne++})
+            countZero>countOne?(height+='0',lower+='1'):(height+='1',lower+='0')
+            values.push(lower,height)
+    return values
+}
+
+const getElement=(table,flag)=>{
+    let index=0
+    while(table.length>1){
+        let swap=calcule(table,index)[flag]
+        table=table.filter(data=>data[index]==swap)
+        index++
+    }
+    return table
+
+}
+
+console.log(parseInt(getElement(table1,1),2)
+*parseInt(getElement(table2,0),2))
+
+console.timeEnd()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
